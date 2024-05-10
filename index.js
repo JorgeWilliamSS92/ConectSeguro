da319d82d1ab643af165326d5fd3cc29e7ddc18c
JorgeWilliamSS92 <143949651+JorgeWilliamSS92@users.noreply.github.com>
2024-05-10 09:16:57 -0300

    Projeto extensão Cruzeiro do Sul - Cybersegurança

---
+++ index.js
@@ -0,0 +1,51 @@
+setTimeout(function () {
+  document.querySelector(`.imgg0`).classList.add("visivel");
+}, 600);
+
+setTimeout(function () {
+  document.querySelector(`.imgg1`).classList.add("visivel");
+}, 800);
+
+setTimeout(function () {
+  document.querySelector(`.imgg2`).classList.add("visivel");
+}, 1000);
+
+setTimeout(function () {
+  document.querySelector(`.imgg3`).classList.add("visivel");
+}, 1200);
+
+setTimeout(function () {
+  document.querySelector(`.imgg4`).classList.add("visivel");
+}, 1400);
+
+setTimeout(function () {
+  document.querySelector(`.imgg5`).classList.add("visivel");
+}, 1600);
+
+setTimeout(function () {
+  document.querySelector(`.imgg6`).classList.add("visivel");
+}, 2600);
+
+setTimeout(function () {
+  document.querySelector(`.imgg7`).classList.add("visivel");
+}, 2400);
+
+setTimeout(function () {
+  document.querySelector(`.imgg8`).classList.add("visivel");
+}, 2200);
+
+setTimeout(function () {
+  document.querySelector(`.imgg9`).classList.add("visivel");
+}, 2000);
+
+setTimeout(function () {
+  document.querySelector(`.imgg10`).classList.add("visivel");
+}, 1800);
+
+setTimeout(function () {
+  document.querySelector(`.video`).classList.add("visivel");
+}, 3000);
+
+setTimeout(function () {
+  document.querySelector(`.navegacao1`).classList.add("visivel");
+}, 3400);
